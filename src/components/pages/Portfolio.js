cbd3dd6c81ffc2108ba638da453b6251c56623de
sendusyourbones <beckee.roos@gmail.com>
2023-07-25 22:04:53 -0700

    Add Learnify to Portfolio and refactor

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,64 +1,90 @@
 import React from 'react';
 
+// Import components
 import Project from '../Project';
-import SneezeTheDay from '../../assets/images/sneeze-the-day-square.jpg';
-import KitchenAlchemy from '../../assets/images/kitchen-alchemy.jpg';
-import TextEditor from '../../assets/images/text-editor.jpg';
-import Scheduler from '../../assets/images/scheduler.jpg';
+
+// Import images
 import CodingQuiz from '../../assets/images/coding-quiz.jpg';
+import Learnify from '../../assets/images/learnify.jpg';
+import KitchenAlchemy from '../../assets/images/kitchen-alchemy.jpg';
+import Scheduler from '../../assets/images/scheduler.jpg';
+import SneezeTheDay from '../../assets/images/sneeze-the-day-square.jpg';
 import WeatherDashboard from '../../assets/images/weather-dashboard.jpg';
+
+// Import CSS
 import '../../assets/styles/Portfolio.css';
 
 export const Portfolio = () => {
+  const projects = [
+    {
+      title: 'Learnify',
+      thumbnail: Learnify,
+      imgAlt: 'Screenshot of the Learnify app',
+      appUrl: 'https://learnify-7e031704dc94.herokuapp.com/',
+      gitHubUrl: 'https://github.com/UserOlena/Learnify',
+    },
+    {
+      title: 'Sneeze The Day',
+      thumbnail: SneezeTheDay,
+      imgAlt: 'Screenshot of the Sneeze the Day app',
+      appUrl: 'https://zed-csp.github.io/dangerForce-Proj-1/',
+      gitHubUrl: 'https://github.com/Zed-CSP/dangerForce-Proj-1',
+    },
+    {
+      title: 'Kitchen Alchemy',
+      thumbnail: KitchenAlchemy,
+      imgAlt: 'Screenshot of the Kitchen Alchemy app',
+      appUrl: 'https://kitchen-alchemy.herokuapp.com/',
+      gitHubUrl: 'https://github.com/Zed-CSP/codeConnoisseurs',
+    },
+    {
+      title: 'Work Day Scheduler',
+      thumbnail: Scheduler,
+      imgAlt: 'Screenshot of the Work Day Scheduler app',
+      appUrl: 'https://sendusyourbones.github.io/daily-planner/',
+      gitHubUrl: 'https://github.com/sendusyourbones/daily-planner',
+    },
+    {
+      title: 'Timed Coding Quiz',
+      thumbnail: CodingQuiz,
+      imgAlt: 'Screenshot of the Timed Coding Quiz app',
+      appUrl: 'https://sendusyourbones.github.io/coding-quiz/',
+      gitHubUrl: 'https://github.com/sendusyourbones/coding-quiz',
+    },
+    {
+      title: '5-Day Weather Dashboard',
+      thumbnail: WeatherDashboard,
+      imgAlt: 'Screenshot of the 5-Day Weather Dashboard app',
+      appUrl: 'https://sendusyourbones.github.io/weather-dashboard/',
+      gitHubUrl: 'https://github.com/sendusyourbones/weather-dashboard',
+    },
+  ];
+
+  function projectList(projects) {
     return (
-        <section>
-            <h1>Portfolio</h1>
-            <div className="d-flex projects-row">
-                <Project
-                    title="Sneeze The Day"
-                    thumbnail={SneezeTheDay}
-                    imgAlt="Screenshot of the Sneeze the Day app"
-                    appUrl="https://zed-csp.github.io/dangerForce-Proj-1/"
-                    gitHubUrl="https://github.com/Zed-CSP/dangerForce-Proj-1"
-                />
-                <Project
-                    title="Kitchen Alchemy"
-                    thumbnail={KitchenAlchemy}
-                    imgAlt="Screenshot of the Kitchen Alchemy app"
-                    appUrl="https://kitchen-alchemy.herokuapp.com/"
-                    gitHubUrl="https://github.com/Zed-CSP/codeConnoisseurs"
-                />
-                <Project
-                    title="Just Another Text Editor"
-                    thumbnail={TextEditor}
-                    imgAlt="Screenshot of the Just Another Text Editor app"
-                    appUrl="https://enigmatic-escarpment-54354-5069d77b2bb8.herokuapp.com/"
-                    gitHubUrl="https://github.com/sendusyourbones/text-editor"
-                />
-                <Project
-                    title="Work Day Scheduler"
-                    thumbnail={Scheduler}
-                    imgAlt="Screenshot of the Work Day Scheduler app"
-                    appUrl="https://sendusyourbones.github.io/daily-planner/"
-                    gitHubUrl="https://github.com/sendusyourbones/daily-planner"
-                />
-                <Project
-                    title="Timed Coding Quiz"
-                    thumbnail={CodingQuiz}
-                    imgAlt="Screenshot of the Timed Coding Quiz app"
-                    appUrl="https://sendusyourbones.github.io/coding-quiz/"
-                    gitHubUrl="https://github.com/sendusyourbones/coding-quiz"
-                />
-                <Project
-                    title="5-Day Weather Dashboard"
-                    thumbnail={WeatherDashboard}
-                    imgAlt="Screenshot of the 5-Day Weather Dashboard app"
-                    appUrl="https://sendusyourbones.github.io/weather-dashboard/"
-                    gitHubUrl="https://github.com/sendusyourbones/weather-dashboard"
-                />
-            </div>
-        </section>
+      <div className='d-flex projects-row'>
+        {projects.map((project) => {
+          const { title, thumbnail, imgAlt, appUrl, gitHubUrl } = project;
+          return (
+            <Project
+              title={title}
+              thumbnail={thumbnail}
+              imgAlt={imgAlt}
+              appUrl={appUrl}
+              gitHubUrl={gitHubUrl}
+            />
+          );
+        })}
+      </div>
     );
+  }
+
+  return (
+    <section>
+      <h1>Portfolio</h1>
+      {projectList(projects)}
+    </section>
+  );
 };
 
 export default Portfolio;
