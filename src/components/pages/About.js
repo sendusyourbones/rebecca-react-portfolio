e657b6459c7ac3923000b0b9f568ccbf4f224eef
sendusyourbones <beckee.roos@gmail.com>
2023-09-08 17:00:08 -0700

    Move proficiencies to About & incorporate Material-UI on pg

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,24 +1,107 @@
 import React from 'react';
+
+// Material-UI imports
 import {
-    Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
 } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+// Asset imports
 import Avatar from '../../assets/images/avatar.png';
 import '../../assets/styles/About.css';
 
-export const About = () => {
-    return (
-        <section id="about-me">
-            <h1>About Me</h1>
+const useStyles = makeStyles({
+  list: {
+    border: '3px solid #94a9ed',
+  },
+  divider: {
+    margin: '3% 0',
+    backgroundColor: 'black',
+  },
+});
 
-            <img 
-                src={Avatar} 
-                alt="Female avatar with medium-length hair, light skin, and brown eyes" 
-            />
-            
-            <p>I grew up in San Diego, California and moved to Berkeley, California in 2007 to complete my undergraduate studies at UC Berkeley. I have worked as a Program Coordinator and a Business Systems Analyst for UC Berkeley Extension. I was lucky enough to be moved into a Web Developer role there before obtaining any formal education and have been learning a lot on the job; I am now attending the Berkeley Coding Boot Camp to learn even more, and I am already putting my learnings to use at work!</p>
-            <p>I love all things puzzles&mdash;jigsaw puzzles, crosswords, and Wordle, to name a few. I enjoy rock climbing, which is a puzzle with your body. I love coding because it's like a puzzle too!</p>
-        </section>
-    );
+export const About = () => {
+  const classes = useStyles();
+
+  return (
+    <section id='about-me'>
+      <Typography variant='h1'>About Me</Typography>
+
+      <img
+        src={Avatar}
+        alt='Female avatar with medium-length hair, light skin, and brown eyes'
+      />
+
+      <Typography paragraph>
+        I grew up in San Diego, California and moved to Berkeley, California in
+        2007 to complete my undergraduate studies at UC Berkeley. I have worked
+        as a Program Coordinator and a Business Systems Analyst for UC Berkeley
+        Extension. I was lucky enough to be moved into a Web Developer role
+        there before obtaining any formal education and have been learning a lot
+        on the job; I am now attending the Berkeley Coding Boot Camp to learn
+        even more, and I am already putting my learnings to use at work!
+      </Typography>
+      <Typography paragraph>
+        I love all things puzzles&mdash;jigsaw puzzles, crosswords, and Wordle,
+        to name a few. I enjoy rock climbing, which is a puzzle with your body.
+        I love coding because it's like a puzzle too!
+      </Typography>
+
+      <Divider variant='middle' className={classes.divider} />
+
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <Typography variant='h2'>Front-End Proficiencies</Typography>
+            <List className={classes.list}>
+              <ListItem divider>
+                <ListItemText align='center'>HTML</ListItemText>
+              </ListItem>
+              <ListItem divider>
+                <ListItemText align='center'>
+                  CSS (media queries, Grid, Flexbox, Bootstrap)
+                </ListItemText>
+              </ListItem>
+              <ListItem divider>
+                <ListItemText align='center'>JavaScript</ListItemText>
+              </ListItem>
+              <ListItem divider>
+                <ListItemText align='center'>jQuery</ListItemText>
+              </ListItem>
+              <ListItem>
+                <ListItemText align='center'>React</ListItemText>
+              </ListItem>
+            </List>
+          </div>
+
+          <div className='col-md-6'>
+            <Typography variant='h2'>Back-End Proficiencies</Typography>
+            <List className={classes.list}>
+              <ListItem divider>
+                <ListItemText align='center'>Express</ListItemText>
+              </ListItem>
+              <ListItem divider>
+                <ListItemText align='center'>Node</ListItemText>
+              </ListItem>
+              <ListItem divider>
+                <ListItemText align='center'>MySQL & Sequelize</ListItemText>
+              </ListItem>
+              <ListItem divider>
+                <ListItemText align='center'>MongoDB & Mongoose</ListItemText>
+              </ListItem>
+              <ListItem>
+                <ListItemText align='center'>REST APIs</ListItemText>
+              </ListItem>
+            </List>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,46 +4,19 @@ import ResumePDF from '../../assets/files/Rebecca-Roos-resume.pdf';
 import '../../assets/styles/Resume.css';
 
 export const Resume = () => {
-    return (
-        <section id="resume">
-            <h1>My Resume</h1>
+  return (
+    <section id='resume'>
+      <h1>My Resume</h1>
 
-            <a 
-                href={ResumePDF} 
-                download="Rebecca-Roos-resume.pdf"
-                className="btn btn-lg btn-resume"
-            >
-                Download my resume
-            </a>
-
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-6">
-                        <h2>Front-End Proficiencies</h2>
-                        <ul className="fs-4 font-monospace">
-                            <li>HTML</li>
-                            <li>CSS (media queries, Grid, Flexbox, Bootstrap)</li>
-                            <li>JavaScript</li>
-                            <li>jQuery</li>
-                            <li>React</li>
-                        </ul>
-                    </div>
-
-                    <div className="col-md-6">
-                        <h2>Back-End Proficiencies</h2>
-                        <ul className="fs-4 font-monospace">
-                            <li>Express</li>
-                            <li>Node</li>
-                            <li>MySQL & Sequelize</li>
-                            <li>MongoDB & Mongoose</li>
-                            <li>REST APIs</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-        </section>
-    );
+      <a
+        href={ResumePDF}
+        download='Rebecca-Roos-resume.pdf'
+        className='btn btn-lg btn-resume'
+      >
+        Download my resume
+      </a>
+    </section>
+  );
 };
 
 export default Resume;
