a1d888cfed2d4c4671d48709e29e4ac3a747d4a0
sendusyourbones <beckee.roos@gmail.com>
2023-09-11 16:58:39 -0700

    Install emailJS. Fix Contact to send email & use MaterialUI.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,116 +1,184 @@
 import React, { useState } from 'react';
-import { TextField } from '@material-ui/core';
 
+// Material-UI imports
+import { Box, Button, TextField, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+// Package imports
+import emailjs from '@emailjs/browser';
+
+// Utility imports
 import { validateEmail } from '../../utils/validateEmail';
+
+// Asset imports
 import '../../assets/styles/Contact.css';
 
+const SubmitButton = withStyles((theme) => ({
+  root: {
+    color: '#000000',
+    backgroundColor: '#B4B4FA',
+    '&:hover': {
+      color: '#FFFFFF',
+      backgroundColor: '#7a7acc',
+    },
+    marginBottom: '1%',
+    border: '2px solid #000000',
+  },
+}))(Button);
+
 export const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const [submitted, setSubmitted] = useState(false);
+  const inputDefaultValues = {
+    value: '',
+    isEmpty: false,
+    isValid: true,
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
+  const [name, setName] = useState(inputDefaultValues);
+  const [email, setEmail] = useState(inputDefaultValues);
+  const [message, setMessage] = useState(inputDefaultValues);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
-        if (name === 'name') {
-            setName(value);
-        } else if (name === 'email') {
-            setEmail(value);
-        } else if (name === 'message') {
-            setMessage(value);
-        }
+  const handleInputChange = (inputValue, setState) => {
+    setState((otherValues) => ({
+      ...otherValues,
+      value: inputValue,
+    }));
+  };
 
-        if (value !== '') {
-            setErrorMessage('');
-        }
+  const handleBlur = (inputValue, setState) => {
+    if (inputValue === '') {
+      setState((otherValues) => ({
+        ...otherValues,
+        isEmpty: true,
+      }));
+    }
+
+    if (setState === setEmail && !validateEmail(inputValue)) {
+      setState((otherValues) => ({
+        ...otherValues,
+        isValid: false,
+      }));
+    }
+  };
+
+  const handleFocus = (state, setState) => {
+    if (state.isEmpty) {
+      setState((otherValues) => ({
+        ...otherValues,
+        isEmpty: false,
+      }));
+    }
+
+    if (!state.isValid) {
+      setState((otherValues) => ({
+        ...otherValues,
+        isValid: true,
+      }));
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
     };
 
-    const handleBlur = (e) => {
-        const { value, name } = e.target;
-
-        if (value === '') {
-            if (name === 'name') {
-                setErrorMessage('Name is required.');
-            } else if (name === 'email') {
-                setErrorMessage('Email address is required.');
-            } else if (name === 'message') {
-                setErrorMessage('Message is required.');
-            }
-        }
-    };
-
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-
-        if (!validateEmail(email)) {
-            setErrorMessage('Please enter a valid email address.');
-            return;
-        }
-
-        setName('');
-        setEmail('');
-        setMessage('');
+    emailjs
+      .send(
+        'service_6w977vf',
+        'template_tgeqq6e',
+        templateParams,
+        '7NjHvxliNrbBPbZ1l'
+      )
+      .then(() => {
+        resetFormFields(setName);
+        resetFormFields(setEmail);
+        resetFormFields(setMessage);
         setSubmitted(true);
-    };
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Apologies, there was an issue sending your message: ${error}. You can email me at beckee.roos@gmail.com.`
+        );
+      });
+  };
 
-    return (
-        <section>
-            <h1>Contact Me</h1>
-            <form>
-                <div className="form-container">
-                    <TextField
-                        label="Name"
-                        value={name}
-                        name="name"
-                        onChange={handleInputChange}
-                        type="text"
-                        variant="outlined"
-                        required
-                        onBlur={handleBlur}
-                        fullWidth
-                    />
-                    <TextField
-                        label="Email Address"
-                        value={email}
-                        name="email"
-                        onChange={handleInputChange}
-                        type="text"
-                        variant="outlined"
-                        required
-                        onBlur={handleBlur}
-                        fullWidth
-                    />
-                    <TextField
-                        label="Message"
-                        value={message}
-                        name="message"
-                        onChange={handleInputChange}
-                        variant="outlined"
-                        multiline
-                        rows={10}
-                        required
-                        onBlur={handleBlur}
-                        fullWidth
-                    />
-                </div>
-                {errorMessage && (
-                    <p className="error">{errorMessage}</p>
-                )}
-                <button
-                    type="button"
-                    onClick={handleFormSubmit}
-                    className="btn btn-lg btn-contact"
-                >
-                    Send Your Message
-                </button>
-                {submitted && (
-                    <p>Your message has been submitted!</p>
-                )}
-            </form>
-        </section>
-    );
+  function resetFormFields(setState) {
+    setState((otherValues) => ({
+      ...otherValues,
+      value: '',
+    }));
+  }
+
+  return (
+    <section>
+      <Typography variant='h1'>Contact Me</Typography>
+      <Typography>
+        To get in touch with me, email me at{' '}
+        <a href='mailto:beckee.roos@gmail.com'>beckee.roos@gmail.com</a> or fill
+        out the form below.
+      </Typography>
+      <Box component='form' onSubmit={handleFormSubmit}>
+        <div className='form-container'>
+          <TextField
+            label='Name'
+            name='name'
+            type='text'
+            variant='outlined'
+            required
+            fullWidth
+            margin='normal'
+            onChange={(e) => handleInputChange(e.target.value, setName)}
+            onBlur={(e) => handleBlur(e.target.value, setName)}
+            onFocus={() => handleFocus(name, setName)}
+            error={name.isEmpty}
+            helperText={name.isEmpty && 'Please enter your name'}
+          />
+          <TextField
+            label='Email Address'
+            name='email'
+            type='text'
+            variant='outlined'
+            required
+            fullWidth
+            margin='normal'
+            onChange={(e) => handleInputChange(e.target.value, setEmail)}
+            onBlur={(e) => handleBlur(e.target.value, setEmail)}
+            onFocus={() => handleFocus(email, setEmail)}
+            error={email.isEmpty || !email.isValid}
+            helperText={
+              (email.isEmpty && 'Please enter your email address') ||
+              (!email.isValid && 'Please enter a valid email address')
+            }
+          />
+          <TextField
+            label='Message'
+            name='message'
+            variant='outlined'
+            multiline
+            rows={10}
+            required
+            fullWidth
+            margin='normal'
+            onChange={(e) => handleInputChange(e.target.value, setMessage)}
+            onBlur={(e) => handleBlur(e.target.value, setMessage)}
+            onFocus={() => handleFocus(message, setMessage)}
+            error={message.isEmpty}
+            helperText={message.isEmpty && 'Please enter a message'}
+          />
+        </div>
+        <SubmitButton type='submit'>Send Your Message</SubmitButton>
+        {submitted && <Typography>Your message has been sent!</Typography>}
+        {errorMessage && (
+          <Typography className='error'>{errorMessage}</Typography>
+        )}
+      </Box>
+    </section>
+  );
 };
 
 export default Contact;
